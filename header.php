72739d7ff780bba182c4317aceb803bc92000420
Colin-baz <colin.bazelaire@gmail.com>
2023-03-24 16:29:43 +0000

    Git final

--- header.php
+++ header.php
@@ -2,12 +2,12 @@
     <a id="logo" href="../index.php"><img src="../images/header_logo.png" alt="logo"></a>
 	<nav>
         <a id="icon_menu" href="#"><img src="images/feen.png" alt=""></a>
-    	<ul>
+    	<ul class="navbar">
             
-        	<li><a href="index.php">Accueil</a></li>
-            <li><a href="listing.php">Discographie</a></li>
-            <li><a href="form_recherche.php">Rechercher</a></li>
-            <li><a href="admin/admin.php">Administration</a></li>
+        	<li class="li1"><a href="index.php">Accueil</a></li>
+            <li class="li2"><a href="listing.php">Discographie</a></li>
+            <li class="li3"><a href="form_recherche.php">Rechercher</a></li>
+            <li class="li4"><a href="admin/admin.php">Administration</a></li>
         </ul>
      </nav>
 </header>
